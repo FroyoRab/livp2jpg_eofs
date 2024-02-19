e93a3dd4436cf65cb0e1983eca28ba91c3be9075
FroyoRab <RabbiTzi0@outlook.com>
2024-02-19 14:47:06 +0800

    feat:add some log.

--- livp2img.py
+++ livp2img.py
@@ -2,18 +2,25 @@
 # filename: getImg2livp.py
 import os
 import sys
+try:
+    from loguru import logger
+except ImportError:
+    import logging as logger
+    logger.basicConfig(level=logger.INFO, stream=sys.stdout)
 
 class ScanLivpImg:
     def __init__(self):
         self.path_list = []
         temp = os.walk(sys.path[0])
+        logger.info("="*6+"查找livp"+"="*6)
         for path,dir_list,file_list in temp:
             for file_name in file_list:
                 if self.__isLivp(file_name):
-                    self.path_list.append(file_name)
+                    logger.info(f'发现livp文件:{path}/{file_name}')
+                    self.path_list.append(f"{path}/{file_name}")
 
-    def __isLivp(self,filePath):
-        if filePath.find(".livp") != -1 or filePath.find(".LIVP") != -1:
+    def __isLivp(self,filename):
+        if filename.split('.')[-1].upper() == 'LIVP':
             return True
 
 FileEofs = {
@@ -22,46 +29,56 @@ FileEofs = {
     'jpg':b'\xff\xd8\xff\xe1',
 }
 
-class GetImg:
-    def __init__(self):
-        file_list = ScanLivpImg().path_list
-        for file_name in file_list:
-            buf = bytearray(os.path.getsize(file_name))
-            with open(file_name, "rb") as row_file:
-                row_file.readinto(buf)
+def convertLivp2Img():
+    file_list = ScanLivpImg().path_list
+    for file_name in file_list:
+        logger.info("="*6+file_name+"="*6)
+        logger.info(f"获取{file_name}二进制内容……")
+        # 读取二进制内容
+        buf = bytearray(os.path.getsize(file_name))
+        with open(file_name, "rb") as row_file:
+            row_file.readinto(buf)
+        
+        file_type = None
+        start_pos = -1
+        # 逐个尝试查找图片文件
+        for one_type in FileEofs.keys():
+            start_pos = buf.find(FileEofs[one_type])
+            if start_pos!=-1:
+                file_type = one_type
+                logger.info(f"识别到livp中包含{one_type}图片,尝试提取……")
+                break
+        else: 
+            logger.exception("Error, can not found jpeg\heic or jpg.")
+            return
+        # zip 格式分文件标志
+        end_post = buf.rfind(b"\x50\x4b\x03\x04")
+        buf = buf[start_pos:end_post]
+        save_file_name = file_name.split('.')[0] + '.' + one_type
+        with open(save_file_name, "wb") as newjpg:
+            newjpg.write(buf)
+        logger.info(f"已提取{save_file_name}。")
             
-            file_type = None
-            start_pos = -1
-            # 逐个尝试查找图片文件
-            for one_type in FileEofs.keys():
-                start_pos = buf.find(FileEofs[one_type])
-                if start_pos!=-1:
-                    file_type = one_type
-                    break
-                
-            else: print("Error, can not found jpeg\heic or jpg.")
-            # zip 格式分文件标志
-            end_post = buf.rfind(b"\x50\x4b\x03\x04")
-            
-            buf = buf[start_pos:end_post]
-            saveFileName = file_name.split('.')[0] + '.' + one_type
-            with open(saveFileName, "wb") as newjpg:
-                newjpg.write(buf)
-                
-            # 尝试heic转换
-            if one_type == "heic" : ConvertHeic2jpg(saveFileName)
+        # 尝试heic转换
+        if one_type == "heic" : 
+            logger.info(f"尝试转换heic文件至jpg……")
+            convertHeic2jpg(save_file_name)
 
-def ConvertHeic2jpg(heicFilePath:str):
+def convertHeic2jpg(heicFilePath:str):
     try:
         from PIL import Image
         from pillow_heif import register_heif_opener
         register_heif_opener()
         #import PIL.Image as PILI
     except ImportError :
-        print("未安装 pillow,pillow_heif 库, 跳过HEIC转换, 请通过 pip install pillow pillow_heif 进行安装。")
+        logger.error("未安装 pillow,pillow_heif 库, 跳过HEIC转换, 请通过 pip install pillow pillow_heif 进行安装。")
         return 
     with Image.open(heicFilePath) as img:
-        img.convert('RGB').save(heicFilePath.split(".")[0]+".jpg")
+        save_file_name = heicFilePath.split(".")[0]+".jpg"
+        img.convert('RGB').save(save_file_name)
+        logger.info(f"已转换heic文件至{save_file_name}。")
     
 if __name__ == "__main__":
-    GetImg()
+    convertLivp2Img()
+    # support cmd exe
+    os.system('pause')
